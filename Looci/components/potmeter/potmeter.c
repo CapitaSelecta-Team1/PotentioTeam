1f33bc4f791ca741cc78dd8277cb6b1d8c00bf75
Jonas Devlieghere <info@jonasdevlieghere.com>
2014-12-15 11:16:45 +0100

    Temp commit

--- Looci/components/potmeter/potmeter.c
+++ Looci/components/potmeter/potmeter.c
@@ -1,7 +1,14 @@
 #include "contiki.h"
 #include "looci.h"
-#include "process.h"
+#include "utils.h"
+#include "timer_utils.h"
+#include <avr/pgmspace.h>
+#include "sensors.h"
 #include "event-types.h"
+#include "adc.h"
+
+#include <stdint.h>
+#include <stdio.h>
 
 #ifdef LOOCI_COMPONENT_DEBUG
 #include "debug.h"
@@ -9,54 +16,30 @@
 #include "nodebug.h"
 #endif
 
+#define LOW_1 0
+#define LOW_2 267
+#define LOW_3 577
+#define LOW_4 841
 
 struct state{
+	int pot;
+	looci_event_t* event;
 };
-const static struct state initVar PROGMEM = {};
 
-
-#define LOOCI_COMPONENT_NAME prototype
 #define LOOCI_NR_PROPERTIES 0
-LOOCI_PROPERTIES(
 
-);
+LOOCI_PROPERTIES();
+COMPONENT_INTERFACES(LIGHT_READING);
+COMPONENT_RECEPTACLES(ANY_EVENT);
+LOOCI_COMPONENT("potmeter", struct state);
+COMPONENT_THREAD( ev, data)
+{
+	COMPONENT_BEGIN(struct state,compState);
+	compState->pot = 0;
 
-COMPONENT_INTERFACES(PROTOTYPE);
-COMPONENT_NO_RECEPTACLES();
-LOOCI_COMPONENT_INIT("Prototype", struct state, &initVar);
+	while(1) {
+		LOOCI_EVENT_RECEIVE(compState->event);
+	}
 
-static uint8_t init(struct state* compState, void* data){
-	PRINTF("Init\r\n");
-	return 1;
+	COMPONENT_END();
 }
-
-
-static uint8_t activate(struct state* compState, void* data){
-	PRINTF("Activate\r\n");
-	return 1;
-}
-
-static uint8_t deactivate(struct state* compState, void* data){
-	PRINTF("Deactivate\r\n");
-	return 1;
-}
-
-static uint8_t sensorCall(struct state* compState, struct sensors_sensor* sensor){
-	PRINTF("Sensor Call Received\r\n");
-	return 1;
-}
-
-static uint8_t eventCall(struct state* compState, core_looci_event_t* event){
-	PRINTF("Event Call Received\r\n");
-	return 1;
-}
-
-//FUNCTION DECLARATION
-
-COMP_FUNCS_INIT
-COMP_FUNC_INIT(init)
-COMP_FUNC_ACTIVATE(activate)
-COMP_FUNC_DEACTIVATE(deactivate)
-COMP_FUNC_SENSOR(sensorCall)
-COMP_FUNC_EVENT(eventCall)
-COMP_FUNCS_END(NULL)
--- Looci/components/potmeter/potmeter.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROTOTYPE_H_
-#define PROTOTYPE_H_
-
-
-#include "component_pub.h"
-DECLARE_LOOCI_COMPONENT(temp_sample);
-
-#endif
