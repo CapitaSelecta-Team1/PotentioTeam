0769dacdbedbca9dcbac50a55dbde899e2314396
Dieter Castel <dietercastel@gmail.com>
2014-12-15 09:37:00 +0100

    potmeter replaced with old develop button.c

--- Contiki/potmeter.c
+++ Contiki/potmeter.c
@@ -6,25 +6,31 @@
 #define LOW_3 577 
 #define LOW_4 841 
 
-
 PROCESS(hello_world_process, "Hello world process");
 AUTOSTART_PROCESSES(&hello_world_process);
 
 PROCESS_THREAD(hello_world_process, ev, data)
 {
-    int p;
 	PROCESS_BEGIN();
 
     printf("Hello, world!\n");
 
+    //Enable all external interupts 
+    // INT0 used for the button
+    EIMSK = 0xff;
+    //Enable listening to all interupts (on rising edge)
+    EICRA = 0x03;
+
 	while(1)
   	{
-  		//PROCESS_WAIT_EVENT();
-        p = readADC(0);
-        printf("Hello ADC, p=%d!\n", p);
-        //Wait for 1000 clock cycles
-        clock_wait(1000);
+  		PROCESS_WAIT_EVENT();
   	}
 
 	PROCESS_END();
+}
+
+ISR(INT0_vect)
+{
+    int p = readADC(0);
+    printf("Hello ADC=%d\n",p);
 }
