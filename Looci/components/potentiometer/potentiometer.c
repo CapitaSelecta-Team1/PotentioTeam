44351c7feb2eb3f45fcc21d3bdb26ad06d155232
Dieter Castel <dietercastel@gmail.com>
2014-12-17 09:57:57 +0100

    Added interpretation from potentiometer value to chipers. Tested and all good.

--- Looci/components/potentiometer/potentiometer.c
+++ Looci/components/potentiometer/potentiometer.c
@@ -35,11 +35,25 @@ static uint8_t event(struct state* compState, core_looci_event_t* event){
 	// Read from ADC
     int potValue = readADC(0);
 
+    // Set 0 as default result chiper.
+    int resultChiper = 0;
+
+    if(potValue >= LOW_1 && potValue < LOW_2){
+        resultChiper = 1;
+    } else if (potValue >= LOW_2 && potValue < LOW_3){
+        resultChiper = 2;
+    } else if (potValue >= LOW_3 && potValue < LOW_4){
+        resultChiper = 3;
+    } else if (potValue >= LOW_4){
+        resultChiper = 4;
+    }
+
     // Print the value, for now
     printf("Potentiometer value = %d\n", potValue);
+    printf("Result Chiper = %d\n", resultChiper);
 
     // Publish value
-	PUBLISH_EVENT(POTENTIO, potValue, sizeof(int));
+	PUBLISH_EVENT(POTENTIO, resultChiper, sizeof(int));
 
     return LC_SUCCESS;
 }
