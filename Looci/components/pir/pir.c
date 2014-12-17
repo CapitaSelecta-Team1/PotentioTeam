e94d81174559a4718ec09067cf251e7519864c9f
SvenAkk <svenakk@hotmail.com>
2014-12-17 10:18:57 +0100

    update pir with energest code

--- Looci/components/pir/pir.c
+++ Looci/components/pir/pir.c
@@ -1,6 +1,7 @@
 #include "contiki.h"
 #include "looci.h"
 #include "event-types.h"
+#include "energest.h"
 
 #ifdef LOOCI_COMPONENT_DEBUG
 #include "debug.h"
@@ -12,10 +13,15 @@ struct state{struct etimer et;};
 
 static bool movement_detected;
 
+static unsigned long rx_start_time;
+static unsigned long lpm_start_time;
+static unsigned long cpu_start_time;
+static unsigned long tx_start_time;
+
 #define LOOCI_COMPONENT_NAME pir_component
 #define LOOCI_NR_PROPERTIES 0
 LOOCI_PROPERTIES();
-COMPONENT_INTERFACES(PIR);
+COMPONENT_INTERFACES( PIR );
 COMPONENT_NO_RECEPTACLES();
 LOOCI_COMPONENT("Pir Component", struct state);
 
@@ -32,6 +38,11 @@ static uint8_t activate(struct state* compState, void* data){
     // Activate timer
     ETIMER_SET(&compState->et, CLOCK_SECOND);
 
+	rx_start_time = energest_type_time(ENERGEST_TYPE_LISTEN);
+	lpm_start_time = energest_type_time(ENERGEST_TYPE_LPM);
+	cpu_start_time = energest_type_time(ENERGEST_TYPE_CPU);
+	tx_start_time = energest_type_time(ENERGEST_TYPE_TRANSMIT);
+
 	return 1;
 }
 
@@ -41,14 +52,20 @@ static uint8_t deactivate(struct state* compState, void* data){
 }
 
 static uint8_t time(struct state* compState, void* data){
-
 	if(movement_detected){
 		printf("PIR component publishing event. \r\n");
 		PUBLISH_EVENT(PIR,true, sizeof(bool));
-		movement_detected = false;
+		movement_detected = false; 
 	}
 
+	printf("energy listen %lu tx %lu cpu %lu lpm %lu\n",
+	energest_type_time(ENERGEST_TYPE_LISTEN) - rx_start_time,
+	energest_type_time(ENERGEST_TYPE_TRANSMIT) - tx_start_time,
+	energest_type_time(ENERGEST_TYPE_CPU) - cpu_start_time,
+	energest_type_time(ENERGEST_TYPE_LPM) - lpm_start_time);
+
 	ETIMER_RESET(&compState->et);
+
 	return 1;
 }
 
@@ -56,6 +73,7 @@ ISR(INT0_vect)
 {
     movement_detected = true;
 }
+
 
 COMP_FUNCS_INIT
 COMP_FUNC_ACTIVATE(activate)
